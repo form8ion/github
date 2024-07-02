59c2a3a2c7b8fe503d07c82fa23413bc49dfe447
Matt Travi <programmer@travi.org>
2024-07-01 23:52:08 -0500

    feat(predicate): enable detecting use of github to version the project

--- example.js
+++ example.js
@@ -3,16 +3,20 @@
 import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import any from '@travi/any';
-import {scaffold} from './lib/index.js';
+import {scaffold, test} from './lib/index.js';
 
 // remark-usage-ignore-next
 stubbedFs({node_modules: stubbedFs.load(resolve('node_modules'))});
 
 // #### Execute
 
+const projectRoot = process.cwd();
+
 await scaffold({
-  projectRoot: process.cwd(),
+  projectRoot,
   name: 'foo',
   owner: 'travi',
   visibility: any.fromList(['Public', 'Private'])
 });
+
+await test({projectRoot});
